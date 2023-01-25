338c6d546f4592d6bcfb13387efa2e0349f7ef99
adimirza <yusufadimirzaman@gmail.com>
2023-01-25 14:23:54 +0700

    memperbaiki collapse menu

--- resources/views/_partials/_footerlib.blade.php
+++ resources/views/_partials/_footerlib.blade.php
@@ -1,7 +1,7 @@
   <!-- Vendor JS Files -->
   <script src="{{ asset('/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
-  <!-- <script src="{{ asset('/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script> -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script> -->
   <script src="{{ asset('/assets/vendor/chart.js/chart.min.js')}}"></script>
   <script src="{{ asset('/assets/vendor/echarts/echarts.min.js')}}"></script>
   <script src="{{ asset('/assets/vendor/quill/quill.min.js')}}"></script>
@@ -9,6 +9,9 @@
   <!-- <script src="{{ asset('/assets/vendor/tinymce/tinymce.min.js')}}"></script> -->
   <script src="{{ asset('/assets/vendor/php-email-form/validate.js')}}"></script>
   <script type="text/javascript" src="{{ asset('/assets/vendor/datatables/datatables.min.js')}}"></script>
+
+  <script src="{{ asset('/assets/js/main.js')}}"></script>
+  <script src="{{ asset('/assets/vendor/jquery/jquery.min.js') }}"></script>
 
   <script>
     $(document).ready( function () {
--- resources/views/_partials/_headerlib.blade.php
+++ resources/views/_partials/_headerlib.blade.php
@@ -14,8 +14,8 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 <!-- Vendor CSS Files -->
-<!-- <link href="{{ asset('/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet"> -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="{{ asset('/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 <link href="{{ asset('/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
 <!-- <link href="{{ asset('/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet"> -->
 <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet">
@@ -28,6 +28,5 @@
 <link href="{{ asset('/assets/css/style.css')}}" rel="stylesheet">
 
   <!-- Template Main JS File -->
-  <script src="{{ asset('/assets/js/main.js')}}"></script>
-  <!-- <script src="{{ asset('/assets/vendor/jquery/jquery.min.js') }}"></script> -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
